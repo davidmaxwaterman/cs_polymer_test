179d2d794ffa4651d22f401df8f5190bd23ec9b7
Max Waterman <max.waterman@intel.com>
2014-12-05 14:06:18 +0000

    changes to show disappearing source

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -5,7 +5,7 @@
   window.addEventListener('polymer-ready', function(e) {
     console.log('CS:received polymer-ready');
 
-    var myElement = document.querySelector('my-element');
+    var myElement = document.createElement('my-element');
 
     myElement.addEventListener('my-property-changed', function(e) {
       console.log('CS:received my-property-changed');
@@ -15,6 +15,9 @@
     myElement.myproperty = 'assigning to property from content script in my-element-ready handler';
     console.log('CS:setting attribute');
     myElement.setAttribute('myproperty','assigning to attribute from content script my-element-ready handler');
+
+    var appendHere = document.getElementById('append-here');
+    appendHere.appendChild(myElement);
   });
 
 })();
