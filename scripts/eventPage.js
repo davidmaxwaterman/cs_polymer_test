cc6eef90937c195d3a8bfd8e6ca12c5a8ca11042
Max Waterman <max.waterman@intel.com>
2014-12-05 13:42:05 +0000

    What's all this then?

--- scripts/eventPage.js
+++ scripts/eventPage.js
@@ -1,18 +1,16 @@
 'use strict';
 
 // Listens for the app launching then creates the window
-/*
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined,function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'http://localhost:8000/' }
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
+
+  chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
+    console.log("EP:tabId:",tabId);
+    console.log("EP:changeInfo:",changeInfo);
+    console.log("EP:tab:",tab);
+
+
+    if (changeInfo.status=='loading' && tab.url=='http://localhost:8000/index.html') {
+      console.log("EP:executing script.");
+      // execute script
+      chrome.tabs.executeScript(null, {file: "scripts/contentScript.js"});
+    }
   });
-});*/
